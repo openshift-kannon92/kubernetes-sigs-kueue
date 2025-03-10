d58896ec97360d8b3d77b073a8445937eb97a4b4
Kevin Hannon <kehannon@redhat.com>
2025-03-10 12:21:13 -0400

    remove covers as we don't care about those

--- Makefile-test-ocp.mk
+++ Makefile-test-ocp.mk
@@ -73,7 +73,6 @@ yq-ocp: ## Download yq locally if necessary.
 
 .PHONY: test-ocp
 test-ocp: ## Run tests.
-<<<<<<< HEAD
 # Configs were filtered out due to a failure
 # Running this in openshift CI we are hitting failures in the unit tests
 # due to how kueue grabs the namespace for default configs
